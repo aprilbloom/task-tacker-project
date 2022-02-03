3487191be0c294a30c36b9c665a58d61d2f4d1e6
lleonor <leahaprilleonor@gmail.com>
2022-02-02 19:58:37 -0600

    Added task and task item components

--- task-tracker/src/app/app.module.ts
+++ task-tracker/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { ResourcesComponent } from './components/resources/resources.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { FormsModule } from '@angular/forms';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ResourcesComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
